31ddb7893918b6d372d89fbb54c21d4bd338b374
Ria <janraecarino@gmail.com>
2014-06-13 09:51:01 +0800

    first commit wordpress

---
+++ wp-admin/includes/gk_installer/sql_dumps/terms.sql
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+INSERT INTO `{$table_prefix}terms` (`term_id`, `name`, `slug`, `term_group`) VALUES (1,'Uncategorized','uncategorized',0),(2,'Main menu','main-menu',0),(6,'post-format-image','post-format-image',0),(7,'Custom Post Formats','custom-post-formats',0),(8,'post-format-aside','post-format-aside',0),(9,'post-format-quote','post-format-quote',0),(10,'post-format-link','post-format-link',0),(11,'post-format-video','post-format-video',0),(12,'Ultrabooks','ultrabooks',0),(13,'Tablet\'s','tablets',0),(14,'TVs','tvs',0),(15,'SmartPhone\'s','smartphones',0),(17,'Tablet','tablet',0),(19,'touchscreen','touchscreen',0),(20,'Android','android',0),(25,'Samsung','samsung',0),(31,'Apple','apple',0),(35,'SmartTV','smarttv',0),(37,'Interface','interface',0),(39,'TV','tv',0),(42,'wireless','wireless',0),(49,'Smartphone','smartphone',0),(51,'2013','2013',0),(60,'technology','technology',0),(68,'Ultrabook','ultrabook',0),(70,'Asus','asus',0),(76,'Windows 8','windows-8',0),(86,'Categories','categories',0),(85,'Footer menu','footer-menu',0),(87,'free wordpress theme','free-wordpress-theme',0),(88,'free blog wordpress theme','free-blog-wordpress-theme',0),(89,'free blog theme','free-blog-theme',0);
+
+
+
+
+
+
+
